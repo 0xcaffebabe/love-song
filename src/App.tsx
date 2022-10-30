c2c77e91d9abe395bb81c2f94cc66b224930c8c6
MY <715711877@qq.com>
2022-10-30 21:33:29 +0800

    初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component<{}, {songList: string}> {
+
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      songList: ''
+    }
+  }
+
+  render() {
+    const { songList } = this.state
+    return (
+      <div>
+        <div>love song</div>
+        <div>{songList}</div>
+      </div>
+    );
+  }
+
+  async componentDidMount() {
+    const data = await (await fetch("./data.txt")).text()
+    this.setState({songList: data})
+    console.log(data)
+  }
 }
 
 export default App;
