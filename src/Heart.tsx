d224edbbf370246ba75a2b78d48b076caf293806
MY <715711877@qq.com>
2024-01-12 13:27:47 +0800

    update

---
+++ src/Heart.tsx
@@ -0,0 +1,9 @@
+import './Heart.css'
+
+export default function Heart() {
+  return <div className="frame">
+  <div className="heart left"></div>
+  <div className="heart right"></div>
+  <div className="heart bottom"></div>
+  </div>
+}
